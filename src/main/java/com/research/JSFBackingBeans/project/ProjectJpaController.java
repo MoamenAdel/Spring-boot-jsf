f46b87cca12c7805d64cba174370daf7162d25e0
MoamenAdel <moamenadel94@gmail.com>
2017-09-30 20:57:23 +0200

    List Project complete cycle

--- src/main/java/com/research/JSFBackingBeans/project/ProjectJpaController.java
+++ src/main/java/com/research/JSFBackingBeans/project/ProjectJpaController.java
@@ -3,31 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.research.JSFBackingBeans;
+package com.research.JSFBackingBeans.project;
 
 import java.io.Serializable;
-import javax.persistence.Query;
-import javax.persistence.EntityNotFoundException;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
+
 
 import org.springframework.context.annotation.Scope;
 
-import com.research.entity.ProjectTypes;
-import com.research.entity.ProjectEmployees;
-import java.util.ArrayList;
-import java.util.Collection;
-import com.research.entity.Lfm;
-import com.research.JSFBackingBeans.exceptions.NonexistentEntityException;
-import com.research.entity.Docs;
 import com.research.entity.Project;
-import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import org.ocpsoft.rewrite.annotation.Join;
 import org.ocpsoft.rewrite.el.ELBeanName;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
@@ -44,7 +29,7 @@ public class ProjectJpaController implements Serializable {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -9006980830134897009L;
+	private static final long serialVersionUID = 9006980830134897009L;
 	Project selected = new Project();
 
 	public Project getSelected() {
