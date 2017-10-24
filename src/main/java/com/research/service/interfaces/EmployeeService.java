08c717d3b1ecc7137cf9693a52d5fde438f9f216
abdelrhman.galal <abdelrhman.tanga@gmail.com>
2017-10-24 21:27:57 +0200

    Upload files employee

--- src/main/java/com/research/service/interfaces/EmployeeService.java
+++ src/main/java/com/research/service/interfaces/EmployeeService.java
@@ -1,13 +1,16 @@
 package com.research.service.interfaces;
 
 import java.util.List;
+
+import org.primefaces.model.UploadedFile;
+
 import com.research.dto.employee.EmployeeDto;
 import com.research.entity.Employee;
 import com.research.service.BaseService;
 import com.research.dto.project.ProjectDto;
 
 public interface EmployeeService extends BaseService<Employee> {
-	EmployeeDto addEmployee(EmployeeDto employeeDto);
+	EmployeeDto addEmployee(EmployeeDto employeeDto, UploadedFile cv, UploadedFile certificate, UploadedFile criminalStatus);
 
 	List<EmployeeDto> getAllEmployees();
 
